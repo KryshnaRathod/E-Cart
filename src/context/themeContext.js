dbd647bd3208ff75fa16b0ec7d11d88faf2fd3a6
KryshnaRathod <krishna.rathod227@gmail.com>
2021-08-20 13:49:18 +0530

    fix problem

---
+++ src/context/themeContext.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const ThemeContext = React.createContext();
+
+export const ThemeProvider = (props) => {
+  const [theme, setTheme] = React.useState("light");
+
+  function changeTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  return (
+    <ThemeContext.Provider value={[theme, changeTheme]}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
